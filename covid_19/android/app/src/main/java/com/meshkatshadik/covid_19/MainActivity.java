e5c918e46474f578bb597e0d96aeb37daab00e30
Meshkat Shadik <shadikmeshkat328@gmail.com>
2020-07-29 13:24:50 +0600

    initial commit

--- covid_19/android/app/src/main/java/com/meshkatshadik/covid_19/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.meshkatshadik.covid_19;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ page_route/android/app/src/main/java/com/meshkatshadik/page_route/MainActivity.java
@@ -0,0 +1,6 @@
+package com.meshkatshadik.page_route;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
